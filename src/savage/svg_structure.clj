f3da110553918fca71c8c49e1f5f228079c07024
Tim Reddehase <robustus@rightsrestricted.com>
2014-12-23 17:08:26 +0100

    add simple functions to provide svg structures.

---
+++ src/savage/svg_structure.clj
@@ -0,0 +1,36 @@
+(ns savage.svg-structure)
+
+(defn svg
+  "Defines a root svg element as a clojure structure"
+  [width height & children]
+  {:element :svg :width width :height height :children children} )
+
+(defn rect
+  "Defines Representation of SVG rectangle"
+  [& attrs]
+  (assoc (apply array-map attrs) :element :rect) )
+
+(defn circle
+  "Defines Representation of SVG circle"
+  [& attrs]
+  (assoc (apply array-map attrs) :element :circle) )
+
+(defn ellipse
+  "Defines Representation of SVG ellipse"
+  [& attrs]
+  (assoc (apply array-map attrs) :element :ellipse) )
+
+(defn line
+  "Defines Representation of SVG line"
+  [& attrs]
+  (assoc (apply array-map attrs) :element :line) )
+
+(defn polyline
+  "Defines Representation of SVG polyline"
+  [& attrs]
+  (assoc (apply array-map attrs) :element :polyline) )
+
+(defn polygon
+  "Defines Representation of SVG polygon"
+  [& attrs]
+  (assoc (apply array-map attrs) :element :polygon) )
