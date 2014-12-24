1324c1203f20b0b1fc7f303089987aa1efa33508
Tim Reddehase <robustus@rightsrestricted.com>
2014-12-24 16:21:40 +0100

    add another simple high-level test
    Additionally adjust the test wrt. to
    the changed root-element structure.

--- test/savage/core_test.clj
+++ test/savage/core_test.clj
@@ -4,10 +4,17 @@
 
 (deftest empty-svg
   (testing "that calling svg with mandatory args will yield empty children"
-    (let [element (svg 64 64)]
+    (let [element (svg {:width 64 :height 64})]
       (is (empty? (:children svg)))))
   (testing "mandatory args will yield those args in result"
-    (let [element (svg 64 65)]
+    (let [element (svg {:width 64 :height 65})]
       (are [x y] (= x y)
-           (:width element) 64
-           (:height element) 65 ))))
+           (get-in element [:attrs :width]) 64
+           (get-in element [:attrs :height]) 65))))
+
+(deftest create-with-no-errors
+  (testing "relative creating of a simple rect-svg"
+    (let [some-rect (rect :width 12 :height 6)
+          res (svg {:width 64 :height 64} some-rect
+                   (left-of-center-from some-rect some-rect 5))]
+      (is (:element-type res) :svg))))
