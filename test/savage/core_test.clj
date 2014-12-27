d997dc5975174de336035ccfc39feca865083bdd
Tim Reddehase <robustus@rightsrestricted.com>
2014-12-27 16:40:56 +0100

    add negative coordinates test for relativity.

--- test/savage/core_test.clj
+++ test/savage/core_test.clj
@@ -9,4 +9,11 @@
           root (svg {:width 64 :height 64} some-rect
                     (left-of-center-from some-rect some-rect 5))]
       (is (some #{relative-rect} (:children root))
-          "Contains the relatively created rect"))))
+          "Contains the relatively created rect")))
+  (testing "relative creation should work with negative coordinates"
+    (let [some-circle (circle :cx -5 :cy -5 :r 10)
+          relative-circle (circle :cx -5 :cy -10 :r 10)
+          root (svg {:width 64 :height 64} some-circle
+                    (above-center-from some-circle some-circle 5))]
+      (is (some #{relative-circle} (:children root))
+          "Contains the relatively created circle"))))
