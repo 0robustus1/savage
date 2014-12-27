b31577961484d5243162fb043c6051e490642aa9
Tim Reddehase <robustus@rightsrestricted.com>
2014-12-27 23:58:04 +0100

    add documentation for public relativity functions.

--- src/savage/core.clj
+++ src/savage/core.clj
@@ -9,6 +9,8 @@
 
 ;; DSL functions
 (defn adjust-position-relatively
+  "Returns a redefined shape by repositioning it according to a source and an
+  offset. It utilizes the virtual center-representation of a svg-shape."
   [shape
    {[source-center-x source-center-y] :center}
    [offset-x offset-y] ]
@@ -16,34 +18,44 @@
     [(+ source-center-x offset-x) (+ source-center-y offset-y)]))
 
 (defn left-of-center-from
+  "Returns redefined target for adjusted relative position. Redefines by
+  applying the offset to the x-axis of source (decreases the x-value)."
   [source target x-offset]
   (adjust-position-relatively target source [(* -1 x-offset) 0]))
 
 (defn right-of-center-from
+  "Returns redefined target for adjusted relative position. Redefines by
+  applying the offset to the x-axis of source (increases the x-value)."
   [source target x-offset]
   (adjust-position-relatively target source [x-offset 0]))
 
 (defn above-center-from
+  "Returns redefined target for adjusted relative position. Redefines by
+  applying the offset to the y-axis of source (decreases the y-value)."
   [source target y-offset]
   (adjust-position-relatively target source [0 (* -1 y-offset)]))
 
 (defn below-center-from
+  "Returns redefined target for adjusted relative position. Redefines by
+  applying the offset to the y-axis of source (increases the y-value)."
   [source target y-offset]
   (adjust-position-relatively target source [0 y-offset]))
 
 (defn- xml-form-child
+  "Traverses the children-tree and returns xml-write syntax."
   [child]
   [(:element-type child) (:attrs child)
    (if (:children child)
      (map xml-form-child (:children child) []))])
 
 (defn export-svg
-  "Exports a svg as xml into a String"
+  "Exports a svg as xml into a String."
   [svg]
   (xml/emit-sexp-str [:svg (:attrs svg)
                       (map xml-form-child (:children svg))]))
 
 (defn eval-export
+  "Evaluates the input-file and write resulting xml to output-file."
   [input-file output-file]
   (let [res (load-file input-file)
         xml (export-svg res)]
